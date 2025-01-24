37cb2cfba613856dcbbdf8b4d20a784a636661b6
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2025-01-24 10:20:19 +0700

    Init project

---
+++ src/main/java/com/example/FuturePath_A/FuturePathAApplication.java
@@ -0,0 +1,13 @@
+package com.example.FuturePath_A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturePathAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturePathAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FuturePath_A/FuturePathAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FuturePath_A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturePathAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
